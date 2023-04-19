437956b1e92815909d6803c6e222a746a121cd53
Stuart Douglas <stuart.w.douglas@gmail.com>
2023-04-19 15:55:21 +1000

    Initial

---
+++ src/test/java/com/notsuspicious/BugExampleTest.java
@@ -0,0 +1,12 @@
+package com.notsuspicious;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BugExampleTest {
+
+    @Test
+    public void testSomeBugInCoolLibrary() {
+        Assertions.assertTrue(true);
+    }
+}
